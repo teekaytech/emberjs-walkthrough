c2af0ce013e15e8babbb74bd1829bfdaa536a97e
Taofeek Olalere <olaleretaofeek@live.com>
2024-02-07 08:27:14 +0100

    Use emberdata model with adapter and serializer

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,9 @@
 import Route from '@ember/routing/route';
-const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+import { service } from '@ember/service'; //import the service decorator from the @ember/service package
 
 export default class IndexRoute extends Route {
-  async model() {
-    let response = await fetch('/api/rentals.json');
-    let { data } = await response.json();
-    return data.map((model) => {
-      let { id, attributes } = model;
-      let type;
-      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-        type = 'Community';
-      } else {
-        type = 'Standalone';
-      }
-
-      return { id, type, ...attributes };
-    });
+  @service store; //injects the store service into the rental route
+  async model(params) {
+    return this.store.findAll('rental', params.rental_id);
   }
 }
--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,20 +1,10 @@
 import Route from '@ember/routing/route';
-
-const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+import { service } from '@ember/service';
 
 export default class RentalRoute extends Route {
+  @service store; //injects the store service into the rental route
+
   async model(params) {
-    let response = await fetch(`/api/rentals/${params.rental_id}.json`);
-    let { data } = await response.json();
-    let { id, attributes } = data;
-    let type;
-
-    if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-      type = 'Community';
-    } else {
-      type = 'Standalone';
-    }
-
-    return { id, type, ...attributes };
+    return this.store.findRecord('rental', params.rental_id);
   }
 }
